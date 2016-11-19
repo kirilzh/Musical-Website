1547db1afae4bc9327cceb3023d86e4b83329a03
Kiril Zhelyazkov <Kiril Zhelyazkov>
2016-11-19 23:11:01 +0200

    added musical in media, minor fixes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
 /* Fading Navbar*/
 
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 768) {
-        $('.navbar').fadeIn();
-    }
-    else {
-        $('.navbar').fadeOut();
-    }
-},false);
+// window.addEventListener("scroll", function() {
+//     if (window.scrollY > 768) {
+//         $('.navbar').fadeIn();
+//     }
+//     else {
+//         $('.navbar').fadeOut();
+//     }
+// },false);
+
+// END Fading Navbar
 
 
 // When the window has finished loading create our google map below
 
 function initMap() {
-  var uluru = {lat: 42.011934, lng: 23.094925};
+  var uluru = {lat: 42.4662471, lng: 23.5069981};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: uluru
   });
+  //
+  // var selo = new google.maps.Map(document.getElementById('selo'), {
+  //   center: uluru
+  // }
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
 }
-
 // ========== END GOOGLE MAP ========== //
