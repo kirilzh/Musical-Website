9d245d7ac5fc9d50b97505c6f42555475eba18e1
Kiril Zhelyazkov <Kiril Zhelyazkov>
2016-11-19 07:55:34 +0200

    Added google maps API, fixed sponsor spot.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 /* Fading Navbar*/
 
 window.addEventListener("scroll", function() {
-    if (window.scrollY > 20) {
+    if (window.scrollY > 768) {
         $('.navbar').fadeIn();
     }
     else {
         $('.navbar').fadeOut();
     }
 },false);
+
+
+// When the window has finished loading create our google map below
+
+function initMap() {
+  var uluru = {lat: 42.011934, lng: 23.094925};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+// ========== END GOOGLE MAP ========== //
